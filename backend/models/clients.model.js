54590aac3012c6f7119173bbacaf46ea86554537
AFKD98 <20100204@lums.edu.pk>
2020-04-20 22:01:42 +0500

    extracted backend commited

---
+++ backend/models/clients.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ClientsSchema = new Schema(
+  {
+    //not much validations
+    ClientName: { type: String, required: true },
+    ContactNumber: { type: String, required: true },
+    Email: { type: String, required: true },
+    Location: { type: String, required: true },
+    Category: { type: String, required: true }, //check number of categories
+    Photographer: { type: String, required: true }, //check id
+    Budget: { type: Number, required: true },
+    Expertise: { type: String, required: true },
+    Event_Description: { type: String, required: true },
+    Approved: { type: String, required: true },
+    date: { type: Date, required: true },
+  },
+  {
+    timestamps: true, //creation and modification timestamps
+  }
+);
+
+const Clients = mongoose.model("Clients", ClientsSchema);
+module.exports = Clients;
