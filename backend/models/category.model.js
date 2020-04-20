a0d1862e329c354ad76468f2738fdc371351b3e4
haider <haiderrana45@gmail.com>
2020-04-21 00:06:27 +0500

    categories updated

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const categorySchema = new Schema(
   {
     categoryname: { type: String, required: true },
-    photographers: [{ photographerID: String }], //multiple photographers
+    photographers: [String], //multiple photographers
   },
   {
     timestamps: true, //creation and modification timestamps
