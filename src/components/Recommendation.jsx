90556f397ba1faaa1e1fa458554ce3f2691f23a3
shahmeer1999 <shahmeer1998@yahoo.com>
2020-04-27 04:48:11 +0500

    fixed recommendation

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-// import styled from "styled-components";
-import Recphoto from "../assets/Recommend.jpg";
 import axios from "axios";
-
+import Form from 'react-bootstrap/Form';
+import {Row, Col, Grid} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import "../styles/photographerRegisteration.css";
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
@@ -54,83 +55,120 @@ class MyForm extends React.Component {
     this.setState({ [nam]: val });
   };
   render() {
-    return (
-      <React.Fragment>
-        {/* //jumborton board and image */}
-        <div
-          style={{
-            backgroundImage: "url(" + Recphoto + ")",
-            paddingTop: 50,
-            padding: 50,
-            paddingBottom: 50,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            width: "100vw",
-            height: "70vh",
-          }}
-          className="jumbotron jumbotron-fluid"
-          className="text-right p-5"
-        >
-          <div className="container">
-            <h3 className="text-center text-white pr-auto pt-auto pb-auto mt-auto">
-              Hello {this.state.username} <br /> please enter your info below
-            </h3>
-            <h1
-              className="display-4"
-              className="text-center text-white pr-auto pt-auto pb-auto mt-auto"
-            >
-              Recommendation
-            </h1>
-            <p
-              className="lead medium"
-              className="text-center text-white pr-auto mt-auto"
-            >
-              Fill the form below and we will get back to you with a<br />
-              photographer that best suits your needs. You'll have a<br />
-              photographer in less than 24 hours!
-              <br />
-            </p>
-          </div>
+    return(
+        <div>
+            {/* START JUMBOTRON */}
+            <div className = "jumbotron jumbotron-fluid one">
+                <div className = "container">
+                    
+                </div>
+            </div>
+            {/* END JUMBOTRON */}
+
+            <div className="container farm">
+                <Form>
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="fullname">
+                                <Form.Label>Full Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter name"/>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="email" className="pr-2">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                        </Col>
+
+                        <Col>
+                            <Form.Group controlId="contact" className="pl-2">
+                                <Form.Label>Contact Number</Form.Label>
+                                <Form.Control type="text" placeholder="Enter contact number" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Col>
+                            <Form.Group controlId="location" className="pr-2">
+                                <Form.Label>Location</Form.Label>
+                                <Form.Control type="text" placeholder="Enter location" />
+                            </Form.Group>
+                        </Col>
+
+                        <Col>
+                            <Form.Group controlId="budget" className="pl-2">
+                                <Form.Label>Budget</Form.Label>
+                                <Form.Control type="text" placeholder="Enter budget"/>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+
+                    <Form.Row>
+                        <Form.Label className="pb-2">Categories (tick all that apply) </Form.Label>
+                    </Form.Row>
+                    <Form.Row className="pb-3">
+                        <Form.Check className="ml-2" label="Wedding" type="checkbox" id="wedding" />
+                        <Form.Check className="ml-4" label="Product" type="checkbox" id="product" />
+                        <Form.Check className="ml-4" label="Event" type="checkbox" id="event" />
+                        <Form.Check className="ml-4" label="Portrait" type="checkbox" id="portrait" />
+                        <Form.Check className="ml-4" label="Ranaography" type="checkbox" id="ranaography" />
+                    </Form.Row>  
+
+                    <Form.Row>
+                        <Col className="col-6">
+                            <Form.Group controlId="date">
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control type="date" placeholder="mm/dd/yyyy"/>
+                            </Form.Group>
+                        </Col>
+
+                        <Col>
+                            <Form.Group controlId="expertise">
+                                <Form.Label>Expertise</Form.Label>
+                                <Form.Control as="select" value="Expertise...">
+                                    <option>1 (beginner)</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    <option>6</option>
+                                    <option>7</option>
+                                    <option>8</option>
+                                    <option>9</option>
+                                    <option>10 (expert)</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                            
+                        
+                    </Form.Row>
+                    
+                    <Form.Row>
+                      <Col>
+                        <Form.Group>
+                          <Form.Label>Comments</Form.Label>
+                          <Form.Control as="textarea" rows="3"/>
+                        </Form.Group>
+                      </Col>
+                    </Form.Row>
+                    
+                    <Form.Row>
+                        <Button className="mt-3" variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form.Row>
+                </Form>
+            </div>
+
+            
+            <br/>
         </div>
-        {/* //Form for getting info starts here */}
-        <form onSubmit={this.onSubmitHandler}>
-          <p>Full Name</p>
-          <input type="text" name="name" onChange={this.onChangeHandler} />
-          <p>Contact Number</p>
-          <input type="text" name="contact" onChange={this.onChangeHandler} />
-          <p>Email</p>
-          <input type="text" name="email" onChange={this.onChangeHandler} />
-          <p>Category</p>
-          <input type="text" name="category" onChange={this.onChangeHandler} />
-          <p>Location</p>
-          <input type="text" name="location" onChange={this.onChangeHandler} />
-          <p>Budget</p>
-          <input type="number" name="budget" onChange={this.onChangeHandler} />
-          <p>Expertise</p>
-          <input type="text" name="expertise" onChange={this.onChangeHandler} />
-          <p>Description</p>
-          <input
-            type="text"
-            name="description"
-            onChange={this.onChangeHandler}
-          />
-
-          <p>Date of event</p>
-          <input
-            type="date"
-            data-date=""
-            data-date-format="DD MMMM YYYY"
-            name="date"
-            onChange={this.onChangeHandler}
-          />
-
-          <button type="submit" className="btn btn-outline-primary pd-4 md-4">
-            Submit
-          </button>
-        </form>
-      </React.Fragment>
-    );
+    )
   }
 }
 
