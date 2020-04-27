a26efaca103840c10c23d77415ec4a3ca00fffc0
AFKD98 <20100204@lums.edu.pk>
2020-04-27 07:34:37 +0500

    file structure changed

---
+++ client/build/precache-manifest.ef6fc6a8c797a4bf816377310b4551d1.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33ec7100acd83978436c5d78ff2b0ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd05f0a60e733d2b9579",
+    "url": "/static/css/2.4520814f.chunk.css"
+  },
+  {
+    "revision": "e86a6aab1834d183240e",
+    "url": "/static/css/main.46f24102.chunk.css"
+  },
+  {
+    "revision": "bd05f0a60e733d2b9579",
+    "url": "/static/js/2.bda09fa5.chunk.js"
+  },
+  {
+    "revision": "9d226f5d128ebd9399b83c1a0d67574c",
+    "url": "/static/js/2.bda09fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86a6aab1834d183240e",
+    "url": "/static/js/main.814f4f41.chunk.js"
+  },
+  {
+    "revision": "0ae597172cf284b10d72",
+    "url": "/static/js/runtime-main.e4562ba2.js"
+  },
+  {
+    "revision": "46d3229b5a11c5a2c981e05b6ae04029",
+    "url": "/static/media/222129.46d3229b.jpg"
+  },
+  {
+    "revision": "674d51a97668b810cc0fefa208c4cf03",
+    "url": "/static/media/Recommend.674d51a9.jpg"
+  },
+  {
+    "revision": "5feffaa58c71b1eb71ec03e4fc5ed077",
+    "url": "/static/media/abc.5feffaa5.jpg"
+  },
+  {
+    "revision": "b87e901ca132df1a01c00120f95e9b05",
+    "url": "/static/media/abcd.b87e901c.jpg"
+  },
+  {
+    "revision": "ffb6133bdc31f145a3690c2b56af0cff",
+    "url": "/static/media/anastasia-dulgier-cZ-fHzucmxk-unsplash.ffb6133b.jpg"
+  },
+  {
+    "revision": "c041ce10a15597beb65b72f402f73d90",
+    "url": "/static/media/categories.c041ce10.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "3c345f343970b23d9257882f530a7ea8",
+    "url": "/static/media/home2.3c345f34.jpg"
+  },
+  {
+    "revision": "9d3229b6a5827c96e51fce6b1dd98959",
+    "url": "/static/media/homephoto2.9d3229b6.png"
+  },
+  {
+    "revision": "5ea9010c502a6ffb41bfaa4be82dca9a",
+    "url": "/static/media/homephoto3.5ea9010c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27a397da3b36115997fd7ab136604760",
+    "url": "/static/media/p1.27a397da.jpg"
+  },
+  {
+    "revision": "98c88ba226ef26fa5d1bc85279cf81fe",
+    "url": "/static/media/p2.98c88ba2.jpg"
+  },
+  {
+    "revision": "ed3a84fc5308e9ff9a92faad7791be40",
+    "url": "/static/media/prof1.ed3a84fc.jpeg"
+  },
+  {
+    "revision": "e7d2306ce53821ddd69af41ce1c3647a",
+    "url": "/static/media/sideview.e7d2306c.jpg"
+  },
+  {
+    "revision": "8e952edf8f0fb2de86d53a96949f401c",
+    "url": "/static/media/this2.8e952edf.jpg"
+  },
+  {
+    "revision": "cd4b5e92f8bcadacbdbd832675a00bce",
+    "url": "/static/media/wedding_cover.cd4b5e92.jpg"
+  }
+]);
