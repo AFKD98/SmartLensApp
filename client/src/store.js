ab83dbf2b1be8f4b1e42476d97c3a7554163eb31
AFKD98 <20100204@lums.edu.pk>
2020-07-17 17:59:03 +0500

    heroku errors

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const initialState = {};
 
@@ -16,7 +15,7 @@ const devTools =
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware), devTools)
+  compose(applyMiddleware(...middleware), devTools)
 );
 
 export default store;
